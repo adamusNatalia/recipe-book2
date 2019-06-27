d6b7064f4d63c63a20643a35fbcf3b76f5dd7365
Student 212832 <212832@student.pwr.edu.pl>
2019-06-27 16:26:38 +0200

    Recipe book

---
+++ src/app/shopping-list/shiopping-edit/shiopping-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shiopping-edit',
+  templateUrl: './shiopping-edit.component.html',
+  styleUrls: ['./shiopping-edit.component.css']
+})
+export class ShioppingEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
